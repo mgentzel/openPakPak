da15b9e8f7101b1ca5d79fb620a399f080cf3d0c
Mark Gentzel <markgentzel@gmail.com>
2011-06-24 15:18:59 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pakpak::Application.config.secret_token = '75d322eee145761527f083efc180b5ac81abbe13087abcf3dfd837b78bb4345a86d5a2b154ba1bece93ca34278973b993041b30f985d623768f1bd24d30dcce4'
